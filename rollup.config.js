35d14420b11523f68f9e931582e2ec7fc359da79
Jesse Jenks <JesseEndoJenks@gmail.com>
2026-09-23 13:27:23 -0700

    Update build config
    Updates rollup config to emit multiple versions, and fixes package json
    entrypoints.

--- rollup.config.js
+++ rollup.config.js
@@ -4,11 +4,21 @@ import terser from "@rollup/plugin-terser";
 
 export default {
     input: "src/index.ts",
-    output: {
-        file: "dist/index.js",
-        format: "iife",
-        name: "BidirectionalTyping"
-    },
+    output: [
+        {
+            file: "dist/index.js",
+            format: "iife",
+            name: "BidirectionalTyping",
+        },
+        {
+            file: "dist/index.mjs",
+            format: "es",
+        },
+        {
+            file: "dist/index.cjs",
+            format: "cjs",
+        },
+    ],
     plugins: [
         resolve(),
         typescript(),
